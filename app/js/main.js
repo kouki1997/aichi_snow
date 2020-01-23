38688b5457a18ae99dd26ef4abc1c51883d90fe4
kouki1997 <goukatenrou@gmail.com>
2020-01-23 20:39:41 +0900

    しくったンゴ

--- app/js/main.js
+++ app/js/main.js
@@ -124,7 +124,7 @@ gifImage.src = "images/osiri.gif";
   				      drawStamp(positions,stampGlasses,40, 2.5, -0.3, -0.5); //メガネのスタンプを描画
   				      drawStamp(positions, stampEars2, 33, 3.0, 0.0, -1.8); // 猫の耳
 
-                  /drawStamp(positions, gifImage, 33, 3.0, 0.0, -1.8); // gif画像
+                  //drawStamp(positions, gifImage, 33, 3.0, 0.0, -1.8); // gif画像
 
   				      //＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
 					//psrElement.innerHTML = "score :" + ctrack.getScore().toFixed(4);
